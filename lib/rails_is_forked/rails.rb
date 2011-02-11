b95babf761f2da3b14f9d8ffa56e4a620bebcd80
Kurt Stephens <ks.github@kurtstephens.com>
2011-02-10 19:43:05 -0600

    More docs and naive disconnect before fork example.

--- lib/rails_is_forked/rails.rb
+++ lib/rails_is_forked/rails.rb
@@ -12,18 +12,32 @@ module RailsIsForked
       return if @@once
       @@once = true
 
-if false
-      # Register callback to disconnect connections before forking child processes.
-      proc = RailsIsForked::ForkCallback.add_callback_before_child! do | child_pid |
-        ActiveRecord::Base.connection_handler.clear_all_connections!
-      end
-else
+if true
       # Register callback to call forget connections in child processes.
       proc = RailsIsForked::ForkCallback.add_callback_in_child! do | child_pid |
         ActiveRecord::Base.connection_handler.connection_pools.each_value do | pool |
-          # $stderr.puts "#{$$}: #{pool}.disconnect!"
+          # Naive solution:
+          # pool.disconnect!
+          #
+          # This causes the following error in the parent:
+          # PGError: server closed the connection unexpectedly
+          # This probably means the server terminated abnormally
+          # before or while processing the request.
+
+          # Simplest solution:
           pool.forget_all_connections!
         end
+      end
+else
+      # Naive solution:
+      # Register callback to disconnect connections before forking child processes.
+      #
+      # This cause the parent to fail if fork occurs with a transaction:
+      # Failure/Error: ActiveRecord::Base.transaction do
+      # not connected
+      #
+      proc = RailsIsForked::ForkCallback.add_callback_before_child! do | child_pid |
+        ActiveRecord::Base.connection_handler.clear_all_connections!
       end
 end
       # $stderr.puts "Registered callback #{proc}"
